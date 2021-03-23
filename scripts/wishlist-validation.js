e811da418221a6528ca28c8c306113b215fdd764
Mark Branscombe <spangoss@mac.com>
2021-03-23 23:35:32 +0000

    Wishlists and Honds

---
+++ scripts/wishlist-validation.js
@@ -0,0 +1,13 @@
+function checkValue(wishlistItem) {
+	var inputGroupId = document.getElementById(wishlistItem + '-input-group');
+	var selectId = document.getElementById(wishlistItem);
+	var value =
+		selectId.options[selectId.selectedIndex].value;
+	if (value == 0) {
+		selectId.classList.remove("valid-wishlist-item");
+		inputGroupId.classList.remove("valid-wishlist-item");
+	} else {
+		selectId.classList.add("valid-wishlist-item");
+		inputGroupId.classList.add("valid-wishlist-item");
+	}
+}
