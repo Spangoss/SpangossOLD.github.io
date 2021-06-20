db2a892a91b942c108b761d3e569599e260e5713
Mark Branscombe <spangoss@mac.com>
2021-06-20 15:43:31 +0100

    Tradeskill test

---
+++ scripts/convert-trade-skills.js
@@ -0,0 +1,20 @@
+function convertTradeSkills(id) {
+
+	var data = id.value;
+	alert(data);
+
+
+	var tradeSkillsString = "Enchant Ring - Healing Power(spell = 27926) Enchant Bracer - Spellpower(spell = 27917) 	Enchant Gloves - Major Spellpower(spell = 33997)";
+
+	var tradeSkillsArray = tradeSkillsString.split('\n');
+	alert(tradeSkillsArray.length);
+
+
+
+
+
+
+
+
+
+}
