cefbd172650ddebe3d203f4c4ee1b8954069d3a8
Mark Branscombe <spangoss@mac.com>
2021-06-20 23:47:30 +0100

    TEsting

--- scripts/trade-skills.js
+++ scripts/trade-skills.js
@@ -1,51 +1,21 @@
 var app = angular.module('tradeSkills', []);
-app.controller('c', function ($scope) {
-
-	var c = this;
-
-	$scope.goToSkill = function (id) {
-		alert('https://tbc.wowhead.com/item=27437/' + id);
-	}
+app.controller('tradeSkillsCntrl', function ($scope) {
 
 
+	$scope.submitSkills = function () {
+		alert('Hello');
+		var inputArray = $scope.tradeSkillsInput.split('\n');
+		$scope.data.skillsArray = [];
 
-
-	$scope.tradeSkillInput;
-
-	var tradeSkillArray = [];
-
-	$scope.tradeSkills = [{
-		name: "Enchant Ring - Healing Power",
-		id: 27926,
-		category: 'Enchant Ring'
-	}, {
-		name: "Enchant Bracer - Spellpower",
-		id: 27917,
-		category: 'Enchant Bracer'
-	}, {
-		name: "Enchant Gloves - Major Spellpower",
-		id: 33997,
-		category: 'Enchant Gloves'
-	}, {
-		name: "Enchant Gloves - Major Thingies",
-		id: 111111,
-		category: 'Enchant Gloves'
-	}];
-
-
-
-	/*for (var i = 0; i < tradeSkills.length; i++) {
-		var skillObj = {
-			skillObj.name = '',
-			skillObj.id = '',
-			skillObj.category = ''
+		for (var i = 0; i < inputArray.length; i++) {
+			var skill = {
+				name: inputArray[i].substr(0, inputArray[i].indexOf('(')),
+				id: inputArray[i].split('=').pop().split(')')[0],
+				category: inputArray[i].substr(0, inputArray[i].indexOf('-'))
+			};
+			$scope.data.skillsArray.push(skill);
 		}
-
-		tradeskills.push(skillObj);
-
-
-	} */
-
+	}
 
 
 });
